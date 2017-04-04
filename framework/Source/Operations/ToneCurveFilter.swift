13e9e8c32e531455130a0994dc7be0d5618310a5
Lasha Efremidze <efremidzel@hotmail.com>
2017-04-04 12:50:11 -0700

    Update ToneCurveFilter.swift

--- framework/Source/Operations/ToneCurveFilter.swift
+++ framework/Source/Operations/ToneCurveFilter.swift
@@ -1,3 +1,9 @@
+//  GPUImageACVFile
+//
+//  ACV File format Parser
+//  Please refer to http://www.adobe.com/devnet-apps/photoshop/fileformatashtml/PhotoshopFileFormats.htm#50577411_pgfId-1056330
+//
+
 public class ToneCurveFilter: BasicOperation {
     public init(acv:String) {
         super.init(fragmentShader:GammaFragmentShader, numberOfInputs:1)
